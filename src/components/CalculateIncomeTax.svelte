4b209af950d5cefb7ba495e7b52fa5dec2dab07c
Mauko Quiroga <public@mauko.me>
2025-03-15 00:38:03 +0100

    feat: add calculate income tax component

---
+++ src/components/CalculateIncomeTax.svelte
@@ -0,0 +1,12 @@
+<main class="flex h-screen items-center justify-center">
+  <section
+    class="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow-sm"
+  >
+    <h1 class="px-4 py-5 font-semibold text-gray-900 sm:px-6">
+      RedistributeMe
+    </h1>
+    <p class="px-4 py-5 sm:p-6">
+      Income tax: <span>1234</span>
+    </p>
+  </section>
+</main>
