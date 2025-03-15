4addfcee971cc9b98f6055f1c2fea092f74b0ef0
Mauko Quiroga <public@mauko.me>
2025-03-15 13:13:15 +0100

    chore: add React

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,11 @@ import svelte from '@astrojs/svelte'
 
 import tailwindcss from '@tailwindcss/vite'
 
+import react from '@astrojs/react'
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
+  integrations: [svelte(), react()],
 
   vite: {
     plugins: [tailwindcss()]
--- workshop/02-calculate-disposable-income.md
+++ workshop/02-calculate-disposable-income.md
@@ -1,1 +1,17 @@
 # Calculate Disposable Income
+
+### Add React
+
+```sh
+npx astro add react
+```
+
+Then format and commit:
+
+```sh
+npx prettier --write .
+git add package-lock.json
+git add -p
+git add .
+git commit -m "chore: add React"
+```
